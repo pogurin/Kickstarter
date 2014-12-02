a2969027a72b78e19b8789cea49c0d3b7403e7b2
NATHAN <nathan.ho28@gmail.com>
2014-12-02 17:12:41 -0500

    Added validations for password within User model

--- kickstarter/app/models/user.rb
+++ kickstarter/app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :backed_projects, through: :pledges, class_name: 'Project'
   has_many :owned_projects, class_name: 'Project'
+
+# are all these password validations (7,8) necessary
+  validates :password, length: {minimum: 3}
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
 end
