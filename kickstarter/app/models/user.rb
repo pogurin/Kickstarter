93ddc9ecc4100e61ee5f9b0df72a9f73f1edbbf5
NATHAN <nathan.ho28@gmail.com>
2014-12-02 15:21:42 -0500

    Layed out all the proper associations within the models

--- kickstarter/app/models/user.rb
+++ kickstarter/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :backed_projects, through: :pledges, class_name: 'Project'
+  has_many :owned_projects, class_name: 'Project'
 end
