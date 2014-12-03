6a41459e9fb65b616a623942e32919f3087c767f
Vincent Jim <vjim323@gmail.com>
2014-12-02 22:29:32 -0500

    passwordreset controller sends an email to the user, alerts them to check their email and redirects to the home page

--- kickstarter/app/controllers/password_resets_controller.rb
+++ kickstarter/app/controllers/password_resets_controller.rb
@@ -1,7 +1,9 @@
 class PasswordResetsController < ApplicationController
   def create
   	@user = User.find_by_email(params[:email])
-  	
+
+  	@user.deliver_reset_password_instructions! if @user
+  	redirect_to(root_path, :notice => "Please check your email for further instructions.")
   end
 
   def edit
