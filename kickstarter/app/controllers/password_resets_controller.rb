b6515a99ddc4842fc98baa4552b5896f44bdd852
Vincent Jim <vjim323@gmail.com>
2014-12-02 22:47:32 -0500

    added comments in the reset password controller

--- kickstarter/app/controllers/password_resets_controller.rb
+++ kickstarter/app/controllers/password_resets_controller.rb
@@ -6,6 +6,7 @@ class PasswordResetsController < ApplicationController
   	redirect_to(root_path, :notice => "Please check your email for further instructions.")
   end
 
+  # Reset password form
   def edit
   	@token = params[:id]
   	@user = User.load_from_reset_password_token(params[:id])
@@ -16,7 +17,21 @@ class PasswordResetsController < ApplicationController
   	end
   end
 
+  # Fires when the user has sent the reset password form (clicked on the link)
   def update
-  	
+  	@token = params[:id]
+  	@user = User.load_from_reset_password_token(params[:id])
+
+		if @user.blank?
+  		not_authenticated
+  		return
+  	end
+
+  	@user.password_confirmation = params[:user][:password_confirmation]
+  	if @user.change_password!(params[:user][:password])
+  		redirect_to(root_path, :notice => "Password has been changed!")
+  	else
+  		render :action => "edit"
+  	end  	
   end
 end
