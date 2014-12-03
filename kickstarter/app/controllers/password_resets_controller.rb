a800a2328d9262ac1cc965d89f6e46ee30f53202
Vincent Jim <vjim323@gmail.com>
2014-12-02 22:39:24 -0500

    completed the reset password form on the resetpassword controller

--- kickstarter/app/controllers/password_resets_controller.rb
+++ kickstarter/app/controllers/password_resets_controller.rb
@@ -7,8 +7,16 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  	@token = params[:id]
+  	@user = User.load_from_reset_password_token(params[:id])
+
+  	if @user.blank?
+  		not_authenticated
+  		return
+  	end
   end
 
   def update
+  	
   end
 end
