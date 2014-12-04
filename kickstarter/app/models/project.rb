02859b4a14bc458269a8e02f3e2976bb6d573695
Vincent Jim <vjim323@gmail.com>
2014-12-04 17:33:35 -0500

    added project validation. Presence must be true for all fields and funding goal must be an integer

--- kickstarter/app/models/project.rb
+++ kickstarter/app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ActiveRecord::Base
 	has_many :backers, through: :pledges, class_name: 'User'
 	belongs_to :owner, class_name: 'User'
 	accepts_nested_attributes_for :rewards
+
+	validates :title, :funding_goal, :description, presence: true
+	validates :funding_goal, numericality: { only_integer: true }
+
 end
