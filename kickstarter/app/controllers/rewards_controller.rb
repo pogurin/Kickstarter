9ad78c0589189c17d0f8dd362c47781c9ab96fcd
Eman <ericjboshart@gmail.com>
2014-12-04 18:18:58 -0500

    Filled in my rewards controller, added rewards and pledges model to routes

--- kickstarter/app/controllers/rewards_controller.rb
+++ kickstarter/app/controllers/rewards_controller.rb
@@ -1,2 +1,29 @@
 class RewardsController < ApplicationController
+
+
+def new
+		@reward = Reward.new
+	end
+
+	def create
+		@reward = Reward.new(reward_params)
+		@reward.project_id = current_project.id
+
+		if @reward.save
+			redirect_to project_url, notice:"reward made you sweetiepie!"
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@reward = Reward.find(params[:id])
+	end 
+
+	
+private
+	def reward_params
+	    params.require(:reward).permit(:amount)
+	end
+
 end
