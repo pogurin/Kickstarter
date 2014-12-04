0eb4fa22a4c2a6300652691715588c955ba82ee8
Vincent Jim <vjim323@gmail.com>
2014-12-04 18:12:59 -0500

    added validations for the rewards model

--- kickstarter/app/models/reward.rb
+++ kickstarter/app/models/reward.rb
@@ -1,3 +1,6 @@
 class Reward < ActiveRecord::Base
 	belongs_to :project
+
+	validates :name, :description, :amount, :backer_limit, presence: true
+	validates :backer_limit, :amount, numericality: { only_integer: true }
 end
