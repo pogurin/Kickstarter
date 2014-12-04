21b95aa74b53fc980b94808f523d46330dba6eb9
NATHAN <nathan.ho28@gmail.com>
2014-12-04 14:35:30 -0500

    pointed user model to owner_id foreign_key

--- kickstarter/app/models/user.rb
+++ kickstarter/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
-  has_many :backed_projects, through: :pledges, class_name: 'Project'
-  has_many :owned_projects, class_name: 'Project'
+  has_many :backed_projects, through: :pledges, class_name: 'Project' 
+  has_many :owned_projects, class_name: 'Project', foreign_key: 'owner_id'
+  has_many :pledges
 
 # are all these password validations (7,8) necessary
   validates :password, length: {minimum: 3}
