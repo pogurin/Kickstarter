a25c222a0b1e328edd298664d94bd8440f6b702d
NATHAN <nathan.ho28@gmail.com>
2014-12-05 12:34:11 -0500

    Created all infrastructure to handle basic pledges on product#show

--- kickstarter/app/models/project.rb
+++ kickstarter/app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
 	has_many :rewards
+	has_many :pledges
 	has_many :backers, through: :pledges, class_name: 'User'
 	belongs_to :owner, class_name: 'User' 
 	accepts_nested_attributes_for :rewards
+	accepts_nested_attributes_for :pledges
 end
--- kickstarter/db/schema.rb
+++ kickstarter/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203230848) do
+ActiveRecord::Schema.define(version: 20141205162238) do
 
   create_table "pledges", force: true do |t|
     t.integer  "amount"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20141203230848) do
     t.datetime "updated_at"
     t.string   "photo"
     t.integer  "owner_id"
+    t.integer  "funded"
   end
 
   create_table "rewards", force: true do |t|
