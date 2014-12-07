0926f9db226079f5701517fd2e795ddbaaf0f1c7
Eman <ericjboshart@gmail.com>
2014-12-07 11:25:43 -0500

    removed user_id column from pledges model.  Literally back where I started

--- kickstarter/db/schema.rb
+++ kickstarter/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205162238) do
+ActiveRecord::Schema.define(version: 20141207162353) do
 
   create_table "pledges", force: true do |t|
     t.integer  "amount"
