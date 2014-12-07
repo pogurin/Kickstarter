dcdd5d590770d7d8d13938d4077b7857c9e2b0b1
Vincent Jim <vjim323@gmail.com>
2014-12-07 17:53:19 -0500

    created dropdown for user to select a category when creating a new project and allows the category_id to be passed in

--- kickstarter/app/controllers/projects_controller.rb
+++ kickstarter/app/controllers/projects_controller.rb
@@ -47,6 +47,6 @@ class ProjectsController < ApplicationController
 
   private 
   def project_params
-    params.require(:project).permit(:title, :description, :funding_goal, :start_date, :end_date, rewards_attributes: [:name, :backer_limit, :amount, :_destroy, :description], pledges_attributes: [:amount, :id,])
+    params.require(:project).permit(:title, :description, :funding_goal, :start_date, :end_date, :category_id, rewards_attributes: [:name, :backer_limit, :amount, :_destroy, :description], pledges_attributes: [:amount, :id,])
   end
 end
