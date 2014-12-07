4440f82a54d074373bdcd20ec9109721b81aca53
Vincent Jim <vjim323@gmail.com>
2014-12-07 16:58:16 -0500

    added category column to Project model

---
+++ kickstarter/db/migrate/20141207215731_add_categories_to_projects.rb
@@ -0,0 +1,5 @@
+class AddCategoriesToProjects < ActiveRecord::Migration
+  def change
+    add_column :projects, :category_id, :integer
+  end
+end
--- kickstarter/db/schema.rb
+++ kickstarter/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207215137) do
+ActiveRecord::Schema.define(version: 20141207215731) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20141207215137) do
     t.string   "photo"
     t.integer  "owner_id"
     t.integer  "funded"
+    t.integer  "category_id"
   end
 
   create_table "rewards", force: true do |t|
