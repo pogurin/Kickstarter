2546353e482417d0b5272cdb6c0f9ad1a513a073
Vincent Jim <vjim323@gmail.com>
2014-12-07 17:01:03 -0500

    created belongs_to and has_many association between project and category models

--- kickstarter/app/models/project.rb
+++ kickstarter/app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ActiveRecord::Base
 	has_many :pledges
 	has_many :backers, through: :pledges, class_name: 'User'
 	belongs_to :owner, class_name: 'User' 
+	belongs_to :category
 
 	accepts_nested_attributes_for :rewards
 	accepts_nested_attributes_for :pledges
